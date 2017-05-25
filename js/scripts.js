b0ba5ac99d77a2a91c8db424910447b0f605f2b1
Halle Tempest and Rose Sponseller <rose@>
2017-05-25 15:51:56 -0700

    Added for loop to sort based on when submit button is clicked

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,7 @@
 // Back end logic
 
+var movieArray = [];
+
 function AddedMovie (movie, year, score) {
   this.movie = movie;
   this.year = year;
@@ -17,47 +19,34 @@ $(function() {
       var userYear = parseInt($("#year").val());
       var userScore = parseInt($("#score").val());
 
+      var newMovie = new AddedMovie(userMovie, userYear, userScore);
+      movieArray.push(newMovie);
       multipleScore.push(userScore);
 
-      var newMovie =
-      "<tr><td>"
+// This will add new movie information directly into the table
+    $("#results").append("<tr><td>"
       + userMovie + "</td><td>"
       + userYear + "</td><td>"
-      + userScore + "</td></tr>";
+      + userScore + "</td></tr>");
+  });
 
-      var newScore = new AddedMovie(userMovie, userYear, multipleScore);
+//When sort button is clicked
+  $("#sort").click(function() {
 
-// This will empty out the fields each time the form is submitted
-      $("#movie-form").each(function(){
-        this.reset();
-      });
+//Empties previous results to show new sorted table
+    $("#results").empty();
 
-      console.log(newScore);
-      console.log(multipleScore);
-
-// This will add new movie information directly into the table
-      $("table tbody").append(newMovie);
-
-      // Sorts score
-      // var sortedMovies = multipleScore.slice(0);
-      multipleScore.sort(function(a, b) {
-        return b - a;
-      });
-
-      // console.log(sortedMovies);
-
-      $("#sortScoreButton").on("click", function() {
-        for (i = 0; i < multipleScore.length; i++) {
-          $("table tbody td:nth-child(3)").append( '<tr>' +
-            '<td>' + newMovie[i].score + '</td>'
-            + '</tr>');
-          }
+    var sortedList = movieArray.slice(0);
+      sortedList.sort(function(a, b) {
+      return b.score - a.score;
     });
+
+    for (i =0; i < sortedList.length; i++) {
+      $("#results").append('<tr>' +
+      '<td>' + sortedList[i].movie + '</td>' +
+      '<td>' + sortedList[i].year + '</td>' +
+      '<td>' + sortedList[i].score + '</td>' +
+      '</tr>');
+    }
   });
 });
-
-// Include a basic form to add new movies to the table. Hint: remember your address book app?
-// Tabular data should at minimum include the title of the movie, the year it was released, and of course, its Tomatometer score.
-//
-// First populate your table, then add a button that sorts each row based on score. Hint: use sort()
-// Use CSS to make your app/table look awesome.
