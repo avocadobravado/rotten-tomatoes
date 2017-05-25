d796a8050273ea4221d83f189322ea3f3511c56f
Halle Tempest and Rose Sponseller <rose@>
2017-05-25 14:54:48 -0700

    Attempt to sort

--- js/scripts.js
+++ js/scripts.js
@@ -19,31 +19,43 @@ $(function() {
 
       multipleScore.push(userScore);
 
-      // Sorts score
-      multipleScore.sort(function(b, a) {
-        return b-a
-      });
-
-      var newMovie = "<tr><td>" + userMovie + "</td><td>" + userYear + "</td><td>" + userScore + "</td></tr>";
+      var newMovie =
+      "<tr><td>"
+      + userMovie + "</td><td>"
+      + userYear + "</td><td>"
+      + userScore + "</td></tr>";
 
       var newScore = new AddedMovie(userMovie, userYear, multipleScore);
-
-      console.log(multipleScore);
 
 // This will empty out the fields each time the form is submitted
       $("#movie-form").each(function(){
         this.reset();
       });
 
+      console.log(newScore);
+      console.log(multipleScore);
+
 // This will add new movie information directly into the table
       $("table tbody").append(newMovie);
 
-      $("#sortScoreButton").submit(function(event) {
-        event.preventDefault();
-          $("td:nth-child(3)").text("hi");
-          });
+      // Sorts score
+      // var sortedMovies = multipleScore.slice(0);
+      multipleScore.sort(function(a, b) {
+        return b - a;
+      });
+
+      // console.log(sortedMovies);
+
+      $("#sortScoreButton").on("click", function() {
+        for (i = 0; i < multipleScore.length; i++) {
+          $("table tbody td:nth-child(3)").append( '<tr>' +
+            '<td>' + newMovie[i].score + '</td>'
+            + '</tr>');
+          }
+    });
   });
-  });
+});
+
 // Include a basic form to add new movies to the table. Hint: remember your address book app?
 // Tabular data should at minimum include the title of the movie, the year it was released, and of course, its Tomatometer score.
 //
